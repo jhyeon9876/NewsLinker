2ca6d076561ed205c23eb2cc4f86ee54a4617799
jhyeon <wumangboys@gmail.com>
2024-12-02 16:34:10 +0900

    버그수정

--- Utils/blog_utils.py
+++ Utils/blog_utils.py
@@ -2,13 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class Blog:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def get_text_from_url(url: str):
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        text = str(soup.find(class_='se-main-container').text).replace('\n\n', '')
-        return text
+def get_text_from_url(url: str):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    text = str(soup.find(class_='se-main-container').text).replace('\n\n', '')
+    return text
